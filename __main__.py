727f5fd4ccdc035e7d5654e52a254f7334e57671
danielsioli <danielsioli@gmail.com>
2022-09-05 23:48:44 -0300

    correção do freio acionando sozinho com leitura errada

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import PySimpleGUI
 import PySimpleGUI as sg
 import logging
 import logging.config
@@ -34,15 +35,16 @@ class Janela:
         self.vendor_id = hex(int(self.config['config']['vendor_id'], base=16))
         self.product_id = hex(int(self.config['config']['product_id'], base=16))
         self.interface = int(self.config['config']['interface'])
-        self.armazenar = int(self.config['config']['armazenar'])
+        self.bytes = int(self.config['config']['bytes'])
         self.zona_morta = int(self.config['config']['zona_morta'])
+        self.erro = int(self.config['config']['erro'])
 
     def iniciar(self):
         global jogando
         logging.info(f'Iniciando com o botão: {self.botao}')
         sg.theme('SandyBeach')
         self.window = sg.Window('Hand Break', self.get_layout(), finalize=True)
-        thread_memoria = Thread(target=self.freio_de_mao, args=(self.armazenar, ))
+        thread_memoria = Thread(target=self.freio_de_mao)
         jogando = True
         try:
             thread_memoria.start()
@@ -56,19 +58,21 @@ class Janela:
                     self.vendor_id = hex(int(values[0], base=16))
                     self.product_id = hex(int(values[1], base=16))
                     self.interface = int(values[2])
-                    self.armazenar = int(values[3])
+                    self.bytes = int(values[3])
                     self.zona_morta = int(values[4])
+                    self.erro = int(values[5])
                     self.config['config']['vendor_id'] = str(values[0])
                     self.config['config']['product_id'] = str(values[1])
                     self.config['config']['interface'] = str(values[2])
-                    self.config['config']['armazenar'] = str(values[3])
+                    self.config['config']['bytes'] = str(values[3])
                     self.config['config']['zona_morta'] = str(values[4])
+                    self.config['config']['erro'] = str(values[5])
                     with open(self.config_ini, 'w', encoding='utf-8-sig') as file:
                         self.config.write(file)
                     jogando = False
                     time.sleep(0.5)
                     self.devices.reset()
-                    thread_memoria = Thread(target=self.freio_de_mao, args=(self.armazenar,))
+                    thread_memoria = Thread(target=self.freio_de_mao)
                     jogando = True
                     thread_memoria.start()
                 elif event == 'botao':
@@ -85,7 +89,7 @@ class Janela:
                     jogando = False
                     time.sleep(0.5)
                     self.devices.reset()
-                    thread_memoria = Thread(target=self.freio_de_mao, args=(self.armazenar,))
+                    thread_memoria = Thread(target=self.freio_de_mao)
                     jogando = True
                     thread_memoria.start()
                     logging.info(f'Tecla redefinida para {self.botao}')
@@ -105,9 +109,10 @@ class Janela:
             [sg.Text('Vendor ID', size=label_size), sg.InputText(self.vendor_id, size=input_size)],
             [sg.Text('Product ID', size=label_size), sg.InputText(self.product_id, size=input_size)],
             [sg.Text('Interface', size=label_size), sg.InputText(self.interface, size=input_size)],
+            [sg.Text('Bytes', size=label_size), sg.InputText(self.bytes, size=input_size)],
             [sg.Text('Zona Morta', size=label_size), sg.InputText(default_text=self.zona_morta, size=input_size)],
-            [sg.Text('Armazenar (segundos)', size=label_size), sg.InputText(default_text=self.armazenar, size=input_size)],
-            [sg.Text('Leitura', size=label_size), sg.ProgressBar(max_value=255, orientation='h', size=(27.25, 25), style='vista', key='freio_de_mao')],
+            [sg.Text('Erro', size=label_size), sg.InputText(self.erro, size=input_size)],
+            [sg.Text('Leitura', size=label_size), sg.ProgressBar(max_value=255, orientation='h', size=(27.25, 18), border_width=PySimpleGUI.DEFAULT_BORDER_WIDTH, key='freio_de_mao')],
             [sg.Submit('Salvar Alterações'), sg.Cancel('Parar')]
         ]
         return layout
@@ -123,7 +128,7 @@ class Janela:
             self.key = HotKey.parse('<' + self.botao + '>')[0]
         return False
 
-    def freio_de_mao(self, armazenar=0):
+    def freio_de_mao(self):
         global jogando
         libusb1_backend = usb.backend.libusb1.get_backend(find_library=libusb_package.find_library)
         self.devices = usb.core.find(idVendor=int(self.vendor_id, base=16), idProduct=int(self.product_id, base=16), backend=libusb1_backend)
@@ -142,27 +147,23 @@ class Janela:
             logging.info('Escutando o dispositivo')
             teclado = keyboard.Controller()
             mouse = Controller()
+            intensidade = 0
             while jogando:
-                intensidade = self.devices.read(endpoint_address, 1024*2)[5]
+                data_raw = self.devices.read(endpoint_address, self.bytes)
+                if data_raw[5] != 0 and intensidade >= data_raw[5] - self.erro and intensidade <= data_raw[5] + self.erro:
+                    continue
+                intensidade = data_raw[5]
                 self.window['freio_de_mao'].update(current_count=intensidade)
                 if not apertado and intensidade >= self.zona_morta:
                     teclado.press(self.key)
                     #mouse.scroll(intensidade, 0)
                     apertado = True
-                elif apertado and intensidade <= self.zona_morta - 10:
+                    self.window['freio_de_mao'].update(bar_color=('#046380', '#E6D3A8'))
+                elif apertado and intensidade <= self.zona_morta - self.erro:
                     teclado.release(self.key)
                     #mouse.scroll(-intensidade, 0)
                     apertado = False
-                if armazenar > 0:
-                    data = pd.concat([data, pd.DataFrame.from_dict(
-                        {'hora': [datetime.today()], 'apertado': [intensidade if apertado else np.NaN],
-                        'nao_apertado': [intensidade if not apertado else np.NaN]})], axis=0, ignore_index=True)
-                    if data.loc[len(data.index) - 1, 'hora'] > data.loc[0, 'hora'] + timedelta(seconds=armazenar):
-                        data.drop(index=0, inplace=True)
-                        data.reset_index(drop=True, inplace=True)
-            if armazenar > 0 and not data.empty:
-                logging.info('Armazenando os dados em data.csv')
-                data.to_csv('data.csv', sep=';', decimal=',', encoding='utf-8-sig', index=False)
+                    self.window['freio_de_mao'].update(bar_color=('#046380', '#E6D3A8'))
 
 if __name__ == '__main__':
     global jogando
